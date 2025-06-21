ca54fb287801506a28e26ea86dd49f19538f9147
FanG198202 <fan198202@gmail.com>
2025-06-21 20:49:06 +0800

    Add files via upload

---
+++ MouseClickCounter/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MouseClickCounter")]
+[assembly: AssemblyDescription("滑鼠左鍵點擊次數記錄器")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("fan198202@gmail.com")]
+[assembly: AssemblyProduct("MouseClickCounter")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyFileVersion("0.12.0.0")]
